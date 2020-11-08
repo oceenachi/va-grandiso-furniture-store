d1cccc7a9f875f6d5711162e6a562850b0951e29
oceenachi <phillipfrancisca@gmail.com>
2020-11-08 13:20:47 +0100

    add product sample component

---
+++ src/components/productSample/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageCard from '../verticalImageCard'
+
+const ProductSample = () => {
+    return (
+        <div>
+            <ImageCard/>
+            
+        </div>
+    )
+}
+
+export default ProductSample
