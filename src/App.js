42ba0a7e61a9217e7c45eb07b8cc21fc35a4c934
oceenachi <phillipfrancisca@gmail.com>
2020-11-08 13:11:15 +0100

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Home from "./views/home";
+import { Transition } from "react-transition-group";
+import MidSection from "./views/MidSection";
 
-function App() {
+const duration = 125;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out 0s`,
+};
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+};
+
+function App({ in: inProp }) {
+  const [isViewing, setisViewing] = useState(0);
+  const [timeOut, setTime] = useState();
+
+  const handleDiscClick = (e) => {
+    const id = e.target.id;
+    clearTimeout(timeOut);
+    setisViewing(Number(id));
+  };
+
+  useEffect(() => {
+    const timeOutting = setTimeout(() => {
+      isViewing >= 3 ? setisViewing(0) : setisViewing(isViewing + 1);
+    }, 3000);
+    setTime(timeOutting);
+  }, [isViewing]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Transition
+        in={inProp}
+        timeout={{
+          appear: 100,
+          enter: 300,
+          exit: 300,
+        }}
+      >
+        {(state) => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <Home id={isViewing} handleDiscClick={handleDiscClick} />
+          </div>
+        )}
+      </Transition>
+      <MidSection />
     </div>
   );
 }
