cfc84d4324f78b2fa1782d4f1783bbce87b52407
oceenachi <phillipfrancisca@gmail.com>
2021-02-07 21:07:38 +0100

    create footer component

---
+++ src/views/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../../components/footer'
+
+const FooterSection = () => {
+    return (
+        <div>
+            <Footer/>
+            
+        </div>
+    )
+}
+
+export default FooterSection
